986f4ab648c733eee3aec7baafaf02a56106d975
Raphael Benoliel <raphael2gb@gmail.com>
2023-06-04 13:35:30 +0300

    npm init

---
+++ index.js
@@ -0,0 +1,7 @@
+const app = require('./server');
+
+const port = process.env.PORT || 3002;
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
---
+++ tests/app.test.js
@@ -0,0 +1,13 @@
+const { expect } = require("chai");
+// const request = require("supertest");
+
+describe("avg", () => {
+  describe("#average", () => {
+    it("calculates the average", () => {
+      const grades = [100, 90, 80];
+      const sum = grades.reduce((total, grade) => total + grade, 0);
+      const average = sum / grades.length;
+      expect(average).equal(90);
+    });
+  });
+});
